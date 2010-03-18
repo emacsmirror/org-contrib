emacsmirror/org-contrib

commit adc81d5d830daaff268aab26ba965e463943b4f8

babel: when tangling do not add extension if filename supplied

babel/lisp/org-babel-tangle.el
@@ -109,7 +109,7 @@ exported source code blocks by language."
                                       target-file))
                        (file-name (when base-name
                                     ;; decide if we want to add ext to base-name
-                                    (if (and ext (not (string= (file-name-extension base-name) ext)))
+                                    (if (and ext (string= "yes" tangle))
                                         (concat base-name "." ext) base-name))))
                   ;; ;; debugging
                   ;; (message

commit e7e2db297ca31850f8879128a6a6b5d0d6cfa90c

babel: Fix org-babel-load-file
This was broken by adc81d5d830daaff268aab26ba965e463943b4f8

babel/lisp/org-babel-tangle.el
@@ -54,7 +54,7 @@ file using `load-file'."
            (exported-file (concat base-name ".el")))
       ;; tangle if the org-mode file is newer than the elisp file
       (unless (and (file-exists-p exported-file) (> (age file) (age exported-file)))
-        (org-babel-tangle-file file base-name "emacs-lisp"))
+        (org-babel-tangle-file file exported-file "emacs-lisp"))
       (load-file exported-file)
       (message "loaded %s" exported-file))))
 
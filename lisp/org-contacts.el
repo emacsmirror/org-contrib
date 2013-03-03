emacsmirror/org-contrib

commit 062dc92de9295c9eda0d303ee01d1a0af5e13e0d

org-contacts.el: Started to silent byte-compiler
* contrib/lisp/org-contacts.el: Remove some compile-time warnings by adding the proper declarations.

(org-contacts-anniversaries): Hack around an undefined variable
because I wasn't able to understand how this function works.

(org-contacts-wl-get-from-header-content): Check whether the variable
`wl-summary-buffer-elmo-folder' is bound or not.

lisp/org-contacts.el
@@ -43,6 +43,7 @@
   (require 'org))
 (require 'gnus-util)
 (require 'org-agenda)
+(require 'org-capture)
 
 (defgroup org-contacts nil
   "Options about contacts management."
@@ -131,6 +132,11 @@ This overrides `org-email-link-description-format' if set."
   "Default file for vcard export."
   :group 'org-contacts
   :type 'file)
+
+;; Decalre external functions and variables
+(declare-function wl-summary-message-number "ext:wl-summary" ())
+(declare-function wl-address-header-extract-address "ext:wl-address")
+(declare-function wl-address-header-extract-realname "ext:wl-address")
 
 (defvar org-contacts-keymap
   (let ((map (make-sparse-keymap)))
@@ -501,7 +507,8 @@ Format is a string matching the following format specification:
   (let ((calendar-date-style 'american)
         (entry ""))
     (unless format (setq format org-contacts-birthday-format))
-    (loop for contact in (org-contacts-filter)
+    (loop with date = nil 		; FIXME: prevent a warning
+	  for contact in (org-contacts-filter)
           for anniv = (let ((anniv (cdr (assoc-string
                                          (or field org-contacts-birthday-property)
                                          (caddr contact)))))
@@ -647,7 +654,8 @@ Works from wl-summary-mode and mime-view-mode - that is while viewing email.
 Depends on Wanderlust been loaded."
   (with-current-buffer (org-capture-get :original-buffer)
     (cond
-     ((eq major-mode 'wl-summary-mode) (when wl-summary-buffer-elmo-folder
+     ((eq major-mode 'wl-summary-mode) (when (and (boundp 'wl-summary-buffer-elmo-folder)
+						  wl-summary-buffer-elmo-folder)
                                          (elmo-message-field
                                           wl-summary-buffer-elmo-folder
                                           (wl-summary-message-number)

commit 04018dd1bf2512d2a365709a0d0558dfad342857

orgcontacts.el: Fix `date' being broken in `org-contacts-anniversaries'.
* contrib/lisp/org-contacts.el: Add defvar for date.  Similar to org.el.

  (org-contacts-anniversaries): Setting date to nil breaks the
  function.  Bug was introduced in 062dc92de.

Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

lisp/org-contacts.el
@@ -499,6 +499,7 @@ A group FOO is composed of contacts with the tag FOO."
                ;; show the next heading
                (org-flag-heading nil)))))))
 
+(org-no-warnings (defvar date)) ;; unprefixed, from calendar.el
 (defun org-contacts-anniversaries (&optional field format)
   "Compute FIELD anniversary for each contact, returning FORMAT.
 Default FIELD value is \"BIRTHDAY\".
@@ -512,8 +513,7 @@ Format is a string matching the following format specification:
   (let ((calendar-date-style 'american)
         (entry ""))
     (unless format (setq format org-contacts-birthday-format))
-    (loop with date = nil 		; FIXME: prevent a warning
-	  for contact in (org-contacts-filter)
+    (loop for contact in (org-contacts-filter)
           for anniv = (let ((anniv (cdr (assoc-string
                                          (or field org-contacts-birthday-property)
                                          (caddr contact)))))
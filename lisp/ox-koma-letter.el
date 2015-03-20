emacsmirror/org-contrib

commit a5aca783230712dcdf26a29152c2ab9d0d72e842

ox-koma-letter: Interpret #+SUBJECT.
* ox-koma-letter.el (org-koma-letter-template): Interpret #+SUBJECT.

lisp/ox-koma-letter.el
@@ -630,7 +630,11 @@ holding export options."
 		    (mapconcat #'symbol-name with-subject ","))))
 	(let* ((title-as-subject (plist-get info :with-title-as-subject))
 	       (subject* (org-string-nw-p
-			  (org-export-data (plist-get info :subject) info)))
+			  (org-export-data
+			   (org-element-parse-secondary-string
+			    (plist-get info :subject)
+			    (org-element-restriction 'keyword))
+			   info)))
 	       (title* (and (plist-get info :with-title)
 			    (org-string-nw-p
 			     (org-export-data (plist-get info :title) info))))

commit 313718336f7ff5c7780ec082fdc799fb6ef42224

ox-koma-letter: fix bug introduced in a5aca78
* ox-koma-letter.el (org-koma-letter-template): Handle empty SUBJECT.

lisp/ox-koma-letter.el
@@ -632,7 +632,7 @@ holding export options."
 	       (subject* (org-string-nw-p
 			  (org-export-data
 			   (org-element-parse-secondary-string
-			    (plist-get info :subject)
+			    (or (plist-get info :subject) "")
 			    (org-element-restriction 'keyword))
 			   info)))
 	       (title* (and (plist-get info :with-title)
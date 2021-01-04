emacsmirror/org-contrib

commit 9e43dfadb7e44d8977e85662a4dbcf2eaea6703f

org-contacts.el: Add support for org-id generated link.
* contrib/lisp/org-contacts.el (org-contacts-link-store): Use org-id
store link if org-id-link-to-org-use-id is t.

lisp/org-contacts.el
@@ -1159,11 +1159,13 @@ are effectively trimmed).  If nil, all zero-length substrings are retained."
   "Store the contact in `org-contacts-files' with a link."
   (when (and (eq major-mode 'org-mode)
 	     (member (buffer-file-name) (mapcar 'expand-file-name org-contacts-files)))
-    (let ((headline-str (substring-no-properties (org-get-heading t t t t))))
-      (org-store-link-props
-       :type "org-contact"
-       :link headline-str
-       :description headline-str))))
+    (if org-id-link-to-org-use-id
+	(org-id-store-link)
+      (let ((headline-str (substring-no-properties (org-get-heading t t t t))))
+	(org-store-link-props
+	 :type "org-contact"
+	 :link headline-str
+	 :description headline-str)))))
 
 (defun org-contacts--all-contacts ()
   "Return an alist (name . (file . position)) of all contacts in `org-contacts-files'."

commit 3b18a3bf19252c408e78d224589ce42320ca6e70

org-contacts.el: Only use org-id-store-link if org-id is loaded
Fixes bug introduced in 9e43dfadb that made org-contacts-anniversaries
error if org-id was not loaded.

lisp/org-contacts.el
@@ -1166,7 +1166,7 @@ are effectively trimmed).  If nil, all zero-length substrings are retained."
   "Store the contact in `org-contacts-files' with a link."
   (when (and (eq major-mode 'org-mode)
 	     (member (buffer-file-name) (mapcar 'expand-file-name org-contacts-files)))
-    (if org-id-link-to-org-use-id
+    (if (and (featurep 'org-id) org-id-link-to-org-use-id)
 	(org-id-store-link)
       (let ((headline-str (substring-no-properties (org-get-heading t t t t))))
 	(org-store-link-props
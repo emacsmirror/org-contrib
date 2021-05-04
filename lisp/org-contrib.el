e386b826e014f17a79191525ce575ffede91bd32
Bastien Guerry <bzg@gnu.org>
2021-05-04 21:55:07 +0200

    lisp/org-contrib.el: New file

---
+++ lisp/org-contrib.el
@@ -0,0 +1,32 @@
+;;; org-contrib.el --- Dummy file for presenting metadata
+
+;; Copyright (C) 2021 Bastien Guerry
+
+;; Author: Bastien Guerry <bzg@gnu.org>
+;; Homepage: https://git.sr.ht/~bzg/org-contrib
+;; Package-Requires: ((emacs "25.1"))
+;; Package-Version: 0.0.1
+;; Keywords: org
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary
+
+;; org-contrib.el provides metadata for the (future) the org-contrib
+;; ELPA package.
+
+(provide 'org-contrib)

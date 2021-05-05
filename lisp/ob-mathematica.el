250b6b2f9b3fad121774d72d76fb5c01da1f47be
Bastien Guerry <bzg@gnu.org>
2021-05-05 14:02:54 +0200

    lisp/ob-mathematica.el: Update the Git repository and license

--- lisp/ob-mathematica.el
+++ lisp/ob-mathematica.el
@@ -4,13 +4,13 @@
 
 ;; Authors: Yi Wang
 ;; Keywords: literate programming, reproducible research
-;; Homepage: https://github.com/tririver/wy-els/blob/master/ob-mathematica.el
+;; Homepage: https://github.com/tririver/ob-mathematica/
 
 ;; This file is not part of GNU Emacs.
 ;;
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 2 of the License, or
+;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 ;;
 ;; GNU Emacs is distributed in the hope that it will be useful,

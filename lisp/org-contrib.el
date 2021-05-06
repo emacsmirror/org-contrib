205e09d93ccbf715fce1958c66ec295e9d2ebbec
Bastien Guerry <bzg@gnu.org>
2021-05-06 22:48:51 +0200

    lisp/org-contrib.el: Add a commentary section
    Thanks to Jonas Bernoulli for suggesting these changes.

--- lisp/org-contrib.el
+++ lisp/org-contrib.el
@@ -1,10 +1,10 @@
-;;; org-contrib.el --- Dummy file for presenting metadata
+;;; org-contrib.el --- Add-ons that are no longer distributed with Org-mode
 
 ;; Copyright (C) 2021 Bastien Guerry
 
 ;; Author: Bastien Guerry <bzg@gnu.org>
 ;; Homepage: https://git.sr.ht/~bzg/org-contrib
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (org "9.4.5"))
 ;; Package-Version: 0.0.1
 ;; Keywords: org
 ;; SPDX-License-Identifier: GPL-3.0-or-later
@@ -26,7 +26,30 @@
 
 ;;; Commentary
 
-;; org-contrib.el provides metadata for the (future) the org-contrib
-;; ELPA package.
+;; This package contains add-ons to Org-mode which are not part of GNU
+;; Emacs or of the official Org package.
+
+;; ** This package receive little if no maintainance. **
+
+;; Especially, there is no guaranty that it is compatible with the latest
+;; Org stable version.  Would you would like to volunteer maintaining it?
+;; If so, please send me an email at bzg@gnu.org.
+
+;; If you want to maintain only one or some of these add-ons please send
+;; me an email and consider hosting the add-ons on a separate repository.
+
+;; These files used to live in the Org repository but have been filtered
+;; out of the Org 9.5 release and are stored here for archival purpose.
+;; The contrib/ directory used to contain a scripts/ directory that now
+;; lives on Worg: <https://code.orgmode.org/bzg/worg/src/master/code>.
+
+;;; Note:
+
+;; This file, org-contrib.el, provides metadata for the (future)
+;; org-contrib GNU ELPA package.
+
+;;; Code:
 
 (provide 'org-contrib)
+
+;;; org-contrib.el ends here
